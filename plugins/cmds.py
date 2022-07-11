741912bb1575729acc4f9bdb14627b90e40766f8
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 16:07:56 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -5,4 +5,15 @@ from config import *
 
 @Client.on_message(filters.user(ADMIN) & filters.command(["cmds"]))
 async def link(bot, message):  
-      await message.reply_text('--**Use This Format**-- \n\n **/revoke {invite link}** ')
+      await message.reply_text("""
+✘ Commands Available -
+
+• `/invite`
+    Get Main Chnnel Custom Invite Link.
+
+• `/invite <text/reply to msg/reply to document>`
+    Carbonise the text, with random bg colours.
+
+• `{i}ccarbon <color ><text/reply to msg/reply to document>`
+    Carbonise the text, with custom bg colours.
+""")
