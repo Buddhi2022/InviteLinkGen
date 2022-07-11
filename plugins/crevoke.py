3591fe06dd5d3355df077b43109e934b8f9af4bd
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 16:43:22 +0530

    Create crevoke.py

---
+++ plugins/crevoke.py
@@ -0,0 +1,17 @@
+from pyrogram import *
+from pyrogram.errors import *
+from config import *
+
+
+@Client.on_message(filters.user(ADMIN) & filters.command(["crevoke"]))
+async def link(bot, message):  
+    if len(message.command) == 1:
+      await message.reply_text('--**Use This Format**-- \n\n **/crevoke {chat id or username} {invite link}** ')
+    else:
+      try:
+         link = message.text.split()[1]
+         id =
+         await bot.revoke_chat_invite_link(-1001698267062, invite_link=link)
+         await message.reply_text(f'Succesfuly revoked `{link}`')
+      except:
+         await message.reply_text(f'**ERROR**')
