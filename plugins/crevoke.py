01d9b9f174cd3432448407a4cca3cafd17b59472
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 16:49:39 +0530

    Update crevoke.py

--- plugins/crevoke.py
+++ plugins/crevoke.py
@@ -9,8 +9,8 @@ async def link(bot, message):
       await message.reply_text('--**Use This Format**-- \n\n **/crevoke {chat id or username} {invite link}** ')
     else:
       try:
-         link = message.text.split()[1]
-         iid = message.text.split()[2]
+         link = message.text.split()[2]
+         iid = message.text.split()[1]
          await bot.revoke_chat_invite_link(iid, invite_link=link)
          await message.reply_text(f'Succesfuly revoked `{link}`')
       except:
