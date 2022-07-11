b55a89b44161db8bd2c6309dac65c2a7cbe14c82
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 16:03:00 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -5,5 +5,4 @@ from config import *
 
 @Client.on_message(filters.user(ADMIN) & filters.command(["revoke"]))
 async def link(bot, message):  
-    if len(message.command) == 1:
       await message.reply_text('--**Use This Format**-- \n\n **/revoke {invite link}** ')
