65a265432383083deec09f2632311eaf0e033633
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 16:03:11 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -3,6 +3,6 @@ from pyrogram.errors import *
 from config import *
 
 
-@Client.on_message(filters.user(ADMIN) & filters.command(["revoke"]))
+@Client.on_message(filters.user(ADMIN) & filters.command(["cmds"]))
 async def link(bot, message):  
       await message.reply_text('--**Use This Format**-- \n\n **/revoke {invite link}** ')
