70bb7f958ac1fc6de3e7df857f582178db60a003
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-13 11:02:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ API_ID = int(os.getenv("API_ID", "19611094"))
 API_HASH = os.getenv("API_HASH", "c5hjk;hljkl;lkjlkjkhlkjklhjkljhkjklhjk")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "bn9790=- -= u90=09= 89-")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-ADMIN = [1961057964, 1884885842]
+ADMIN = [5158190595, 1884885842]
